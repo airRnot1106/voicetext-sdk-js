18c17e3034709d95ddddafcc87e4efbedd79c591
airRnot1106 <yngw.rintaro@gmail.com>
2022-03-28 15:49:51 +0900

    エラー処理を追加

--- src/voicetext.ts
+++ src/voicetext.ts
@@ -91,11 +91,26 @@ export class VoiceText {
     }
 
     setSpeaker(speaker: Speaker): this {
+        const speakers: Speaker[] = [
+            'show',
+            'haruka',
+            'hikari',
+            'takeru',
+            'santa',
+            'bear',
+        ];
+        if (!speakers.includes(speaker)) {
+            throw new Error(`Speaker must be one of ${speakers.join(', ')}.`);
+        }
         this._voiceTextParams.speaker = speaker;
         return this;
     }
 
     setFormat(format: Format): this {
+        const formats: Format[] = ['wav', 'ogg', 'mp3'];
+        if (!formats.includes(format)) {
+            throw new Error(`Format must be one of ${formats.join(', ')}.`);
+        }
         this._voiceTextParams.format = format;
         return this;
     }
@@ -103,8 +118,12 @@ export class VoiceText {
     setEmotion(emotion: Emotion): this {
         if (!this._validateParams(this._voiceTextParams)) {
             throw new Error(
-                'Speaker "show" cannot be set to emote. Or the speaker is not set.'
+                'Speaker "show" cannot set emote or the speaker is not set.'
             );
+        }
+        const emotions: Emotion[] = ['happiness', 'anger', 'sadness'];
+        if (!emotions.includes(emotion)) {
+            throw new Error(`Emotion must be one of ${emotions.join(', ')}.`);
         }
         this._voiceTextParams.emotion = emotion;
         return this;
@@ -113,8 +132,12 @@ export class VoiceText {
     setEmotionLevel(level: 1 | 2 | 3 | 4): this {
         if (!this._validateParams(this._voiceTextParams)) {
             throw new Error(
-                'Speaker "show" cannot be set to emote. Or the speaker is not set.'
+                'Speaker "show" cannot set emote or the speaker is not set.'
             );
+        }
+        const levels: EmotionLevel[] = [1, 2, 3, 4];
+        if (!levels.includes(level)) {
+            throw new Error(`Emotion level must be between 1 and 4.`);
         }
         this._voiceTextParams.emotion_level = level;
         return this;
